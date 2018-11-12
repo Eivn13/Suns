cb33b30179de950d85ea18242b5232c5ddbaec6b
eivn13 <dominikpruzinsky@gmail.com>
2018-11-12 21:09:27 +0100

    mlp vol2

--- zad3/uloha3.py
+++ zad3/uloha3.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 from sklearn import svm
+from sklearn import metrics
 from sklearn.neural_network import MLPClassifier
 import random
 import matplotlib.pyplot as plt
@@ -9,29 +10,47 @@ import numpy as np
 
 def mlp(dataset, size):
     clf = MLPClassifier(solver="adam",
-                        hidden_layer_sizes=(1, 5), max_iter=1000, learning_rate_init=0.003, learning_rate="invscaling")
+                        hidden_layer_sizes=(25,),
+                        max_iter=5000,
+                        learning_rate_init=0.0003,
+                        learning_rate="adaptive",
+                        activation='relu',
+                        alpha=0.1,
+                        n_iter_no_change=10,
+                        tol=0.000001,
+                        verbose=1)
     X = []
     y = []
-    n = 1
+    n = 0
     for x in dataset:
         if size < n:
             break
         X.append(x[0])
         y.append(x[1])
         n += 1
-    print(y)
     model = clf.fit(X, y)
     plot_loss = np.asarray(model.loss_curve_)
     plt.plot(plot_loss)
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
-    plt.show()
     global test
-    skuska = clf.predict(test)
+    data = []
+    labels = []
+    for x in test:
+        data.append(x[0])
+        labels.append(x[1])
+    skuska = clf.predict(data)
     print(skuska)
-    print("Loss: 1, " + str(plot_loss[0]) + " 2, " + str(plot_loss[1]) + " 3, " + str(plot_loss[2]) + " ..." +
-          str(len(plot_loss)) + "-th, " + str(plot_loss[-1]))
-    # plt.plot(skuska.)
+    n = 0
+    chyba = 0
+    for label in labels:
+        if label != skuska[n]:
+            chyba += 1
+        n += 1
+    print(  # "Velkost naucneho datasetu: " + str(size) + ", " +
+        "pocet chyb: " + str(chyba))
+    plt.show()
+    pickle.dump(clf, open("./model_clf", "wb"))
 
 
 def support_vector_machine(dataset, size):
@@ -39,12 +58,9 @@ def support_vector_machine(dataset, size):
 
 
 def make_dataset():
-    size = 5000
     size_of_sample = 112
     enddir = os.path.dirname(__file__)[:-4]
     path = enddir + "zad1/Uloha2"
-    statistical_data = []
-    done_classes = []
 
     for clss in os.listdir(path):
         data = pickle.load(open(path + "/" + clss, "rb"))
@@ -55,23 +71,21 @@ def make_dataset():
             for image in data:
                 if n == size_of_sample:
                     break
-                dataset.append(tuple((image.reshape(100*100*3), clss[0])))
+                dataset.append(tuple((image.reshape(100 * 100 * 3), clss[0])))
                 n += 1
             size_of_sample = 104
         else:
             global test
             for image in data:
-                test.append(image.reshape(100*100*3))
+                test.append(tuple((image.reshape(100 * 100 * 3), clss[0])))
 
-        random.shuffle(dataset)
+    random.shuffle(dataset)
 
 
 def compute():
     make_dataset()
-    mlp(dataset, 5)
-    # support_vector_machine(dataset)
-
-    " tu sa bude volat svm 1 svm 2 mlp 1 mlp 2"
+    mlp(dataset, 5000)
+    # support_vector_machine(dataset, 5)
 
 
 dataset = []
