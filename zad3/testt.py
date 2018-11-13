dc7310b8264fe1aac7d0bb140aa536e43a07ef9e
eivn13 <dominikpruzinsky@gmail.com>
2018-11-13 20:14:39 +0100

    plne vypracovane

--- zad3/testt.py
+++ zad3/testt.py
@@ -1,7 +1,7 @@
 import pickle
+import matplotlib.pyplot as plt
 
-
-clf = pickle.load(open("./model_clf_5000", "rb"))
+clf = pickle.load(open("./model_clf_5000_n16", "rb"))
 print(clf)
 data = pickle.load(open("D:/skola/ZS 2018-2019/Suns_pycharm/Suns1/zad1/dataset test", "rb"))
 labels = data["labels test"]
@@ -9,14 +9,15 @@ data = data["dataset test"]
 test = []
 for image in data:
     test.append(image.reshape(100 * 100 * 3))
-
-skuska = clf.predict(test)
-print(skuska)
-n = 0
-chyba = 0
-for label in labels:
-    if label != skuska[n]:
-        chyba += 1
-    n += 1
-print("Z 1920 testovacich vzoriek bolo zle zaklasifikovanych: " + str(chyba)
-      + ", v percentach: " + str(chyba/19.2) + ".")
+for x in range(0, 2):
+    skuska = clf.predict(test)
+    print(skuska)
+    n = 0
+    chyba = 0
+    for label in labels:
+        if label != skuska[n]:
+            chyba += 1
+        n += 1
+    print("Z 1920 testovacich vzoriek bolo zle zaklasifikovanych: " + str(chyba)
+          + ", v percentach: " + str(chyba/19.2) + ".")
+    plt.subplot()
--- zad3/uloha3.py
+++ zad3/uloha3.py
@@ -1,60 +1,96 @@
 import os
 import pickle
 from sklearn import svm
-from sklearn import metrics
 from sklearn.neural_network import MLPClassifier
 import random
 import matplotlib.pyplot as plt
+import cv2
 import numpy as np
 
 
-def mlp(dataset, size):
-    clf = MLPClassifier(solver="adam",
-                        hidden_layer_sizes=(25,),
-                        max_iter=5000,
-                        learning_rate_init=0.0003,
-                        learning_rate="adaptive",
-                        activation='relu',
-                        alpha=0.1,
-                        n_iter_no_change=10,
-                        tol=0.000001,
-                        verbose=1)
-    X = []
-    y = []
-    n = 0
-    for x in dataset:
-        if size < n:
-            break
-        X.append(x[0])
-        y.append(x[1])
-        n += 1
-    model = clf.fit(X, y)
-    plot_loss = np.asarray(model.loss_curve_)
-    plt.plot(plot_loss)
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    global test
-    data = []
-    labels = []
-    for x in test:
-        data.append(x[0])
-        labels.append(x[1])
-    skuska = clf.predict(data)
-    print(skuska)
-    n = 0
-    chyba = 0
-    for label in labels:
-        if label != skuska[n]:
-            chyba += 1
-        n += 1
-    print(  # "Velkost naucneho datasetu: " + str(size) + ", " +
-        "pocet chyb: " + str(chyba))
-    plt.show()
-    pickle.dump(clf, open("./model_clf", "wb"))
+def mlp(dataset, size, hidden_layer):
+    priem_chybovost = 0
+    jump = int(round(size/5))
+    for x in range(0, size, jump):
+        clf = MLPClassifier(solver="adam",
+                            hidden_layer_sizes=hidden_layer,
+                            max_iter=5000,
+                            learning_rate_init=0.0003,
+                            learning_rate="adaptive",
+                            activation='relu',
+                            alpha=0.1,
+                            n_iter_no_change=10,
+                            tol=0.000001)
+        X = []
+        y = []
+        n = 0
+        for j in dataset:
+            if size == n:
+                break
+            X.append(j[0])
+            y.append(j[1])
+            n += 1
+        data = []
+        labels = []
+        data = X[x:x+jump]
+        labels = y[x:x+jump]
+        del X[x:x+jump], y[x:x+jump]
+        model = clf.fit(X, y)
+        # plot_loss = np.asarray(model.loss_curve_)
+        # plt.plot(plot_loss)
+        # plt.xlabel("Epoch")
+        # plt.ylabel("Loss")
+        # plt.show()
+        skuska = clf.predict(data)
+        n = 0
+        chyba = 0
+        for label in labels:
+            if label != skuska[n]:
+                chyba += 1
+            n += 1
+
+        priem_chybovost += chyba/(jump/100)
+        print("Velkost trenovacich dat: " + str(size-jump) + ", " + "pocet chyb: " + str(chyba))
+        pickle.dump(clf, open("./model_clf_" + str(size) + "_" + str(hidden_layer), "wb"))
+
+    return 100 - (priem_chybovost / 5)
 
 
-def support_vector_machine(dataset, size):
-    print("hello")
+def support_vector_machine(dataset, size, krnl):
+    priem_chybovost = 0
+    jump = int(round(size / 5))
+    for x in range(0, size, jump):
+        X = []
+        y = []
+        n = 0
+
+        for j in dataset:
+            if size == n:
+                break
+            X.append(j[0])
+            y.append(j[1])
+            n += 1
+        data = []
+        labels = []
+        data = X[x:x + jump]
+        labels = y[x:x + jump]
+        del X[x:x + jump], y[x:x + jump]
+
+        clf = svm.SVC(kernel=krnl, cache_size=4000)
+        clf.fit(X, y)
+        skuska = clf.predict(data)
+        n = 0
+        chyba = 0
+        for label in labels:
+            if label != skuska[n]:
+                chyba += 1
+            n += 1
+
+        priem_chybovost += chyba/(jump/100)
+        print("Velkost trenovacich dat: " + str(size) + ", " + "pocet chyb: " + str(chyba))
+        pickle.dump(clf, open("./model_clf_svm_" + krnl, "wb"))
+
+    return 100 - (priem_chybovost / 5)
 
 
 def make_dataset():
@@ -71,23 +107,45 @@ def make_dataset():
             for image in data:
                 if n == size_of_sample:
                     break
-                dataset.append(tuple((image.reshape(100 * 100 * 3), clss[0])))
+                tmp = cv2.resize(image, (0, 0), fx=0.5, fy=0.5)
+                dataset.append(tuple((tmp.reshape(50 * 50 * 3), clss[0])))
                 n += 1
             size_of_sample = 104
-        else:
-            global test
-            for image in data:
-                test.append(tuple((image.reshape(100 * 100 * 3), clss[0])))
 
     random.shuffle(dataset)
 
 
 def compute():
     make_dataset()
-    mlp(dataset, 5000)
-    # support_vector_machine(dataset, 5)
+    one_hidden = tuple((25, ))
+    n_hidden = tuple((50, 40, 30))
+    ds = [50, 100, 200, 1000, 5000]
+    array = [mlp(dataset, ds[0], one_hidden), mlp(dataset, ds[1], one_hidden), mlp(dataset, ds[2], one_hidden),
+             mlp(dataset, ds[3], one_hidden), mlp(dataset, ds[4], one_hidden)]
+    plt.ylabel("Accuracy")
+    plt.xlabel("Size of training dataset")
+    plt.plot([50, 100, 200, 1000, 5000], array)
+
+    del array[:]
+    array = [mlp(dataset, ds[0], n_hidden), mlp(dataset, ds[1], n_hidden), mlp(dataset, ds[2], n_hidden),
+             mlp(dataset, ds[3], n_hidden), mlp(dataset, ds[4], n_hidden)]
+    plt.plot([50, 100, 200, 1000, 5000], array)
+
+    del array[:]
+    array = [support_vector_machine(dataset, ds[0], "linear"), support_vector_machine(dataset, ds[1], "linear"),
+             support_vector_machine(dataset, ds[2], "linear"), support_vector_machine(dataset, ds[3], "linear"),
+             support_vector_machine(dataset, ds[4], "linear")]
+    plt.plot([50, 100, 200, 1000, 5000], array)
+
+    del array[:]
+    array = [support_vector_machine(dataset, ds[0], "rbf"), support_vector_machine(dataset, ds[1], "rbf"),
+             support_vector_machine(dataset, ds[2], "rbf"), support_vector_machine(dataset, ds[3], "rbf"),
+             support_vector_machine(dataset, ds[4], "rbf")]
+    plt.plot([50, 100, 200, 1000, 5000], array)
+
+    plt.legend(["1 hidden layer with 25 neurons", "3 hidden layers with 50, 40, 30 neurons",
+                "SVM linear", "SVM rbf"])
 
 
 dataset = []
-test = []
 compute()
